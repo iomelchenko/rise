958531fe2179e1b5348a746119733427fd9c338a
succesom <omelchenko_ig@mail.ru>
2013-04-14 16:26:26 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rise::Application.config.secret_token = 'd2b0012f67c642312a77139bbcdb5d35a3a679589d4c8ae2614f9c17e7108f46af9a705445794ba602695796ece765090f9e6d4db76f610f382cfeec2c0f6e75'
