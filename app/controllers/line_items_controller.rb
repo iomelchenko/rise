8a21e43a82bb6727c7cc8dbb9825ddac396a9fa0
succesom <omelchenko_ig@mail.ru>
2013-05-04 19:39:56 +0300

    Cart with AJAX

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,7 +47,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_path }
+        format.js { @current_item = @line_item }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
@@ -75,11 +76,13 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    @cart = current_cart
     @line_item = LineItem.find(params[:id])
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to store_index_path }
+
       format.json { head :no_content }
     end
   end
